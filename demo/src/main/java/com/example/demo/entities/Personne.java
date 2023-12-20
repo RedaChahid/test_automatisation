9b83609c553dba76c0b67a0081c10bb64b9deef3
reda <redachahid59@gmail.com>
2023-12-21 00:57:37 +0100

    Premier commit

---
+++ demo/src/main/java/com/example/demo/entities/Personne.java
@@ -0,0 +1,19 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor@NoArgsConstructor
+public class Personne {
+    @jakarta.persistence.Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String Firstname;
+    private String Lastname;
+}   
